73f13f2d9012593a0e551d48af26d2410d2984d2
lihansunbai <lihansunbai@163.com>
2016-05-01 16:53:22 +0800

    Set crawl delay

--- SpiderGanji/SpiderGanji/settings.py
+++ SpiderGanji/SpiderGanji/settings.py
@@ -24,7 +24,8 @@ NEWSPIDER_MODULE = 'SpiderGanji.spiders'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY=3
+DOWNLOAD_DELAY=3
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
